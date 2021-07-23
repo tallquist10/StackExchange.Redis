c823583e160510fda8c2a173fc00ade665bba4a8
Caleb Tallquist <caleb.tallquist@hope.edu>
2021-07-23 10:26:28 -0400

    Enums up to Exclude

---
+++ StackExchange.Redis.FSharp/Enums.fs
@@ -0,0 +1,70 @@
+﻿namespace StackExchange.Redis.FSharp
+
+module Enums =
+    type Aggregate =
+    | Min
+    | Max
+    | Sum
+
+    type Bitwise =
+    | And
+    | Or
+    | Xor
+    | Not
+
+    type ClientFlags =
+    | None
+    | ReplicaMonitor
+    | Replica
+    | Master
+    | Transaction
+    | Blocked
+    | TransactionDoomed
+    | Closing
+    | Unblocked
+    | CloseASAP
+    | PubSubSubscriber
+    | ReadOnlyCluster
+    | UnixDomainSocket
+
+    type ClientType =
+    | Normal
+    | Replica
+    | PubSub
+
+    type CommandFlags =
+    | None
+    | FireAndForget
+    | PreferMaster
+    | DemandMaster
+    | PreferReplica
+    | DemandReplica
+    | NoRedirect
+    | NoScriptCache
+
+    type CommandStatus =
+    | Unknown
+    | WaitingToBeSent
+    | Sent
+
+    type ConnectionFailureType =
+    | None
+    | UnableToResolvePhysicalConnection
+    | SocketFailure
+    | AuthenticationFailure
+    | ProtocolFailure
+    | InternalFailure
+    | SocketClosed
+    | ConnectionDisposed
+    | Loading
+    | UnableToConnect
+
+    type ConnectionType =
+    | None
+    | Interactive
+    | Subscription
+
+    type Exclude =
+    | None
+    | Start
+    | Stop
